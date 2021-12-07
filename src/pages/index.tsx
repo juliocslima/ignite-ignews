f4a1948b7057a6b6cf89ea33b65870466191c4bd
Julio Lima <lima.julio.cs@gmail.com>
2021-12-07 10:04:08 -0300

    Home page and SubscribeButton component created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
+
+import styles from './home.module.scss';
 
 export default function Home() {
   return ( 
@@ -7,9 +10,15 @@ export default function Home() {
         <title>Início | ig.news</title>
       </Head>
       
-      <main>
-        <section>
-
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all the publication <br/>
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
