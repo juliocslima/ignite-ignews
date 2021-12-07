e5c7c743663fdf4bbc6f6fc43a8c8efbf65bd5b9
Julio Lima <lima.julio.cs@gmail.com>
2021-12-07 18:16:02 -0300

    API next-auth with Github implemented

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,26 +1,29 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
 
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
-    className={styles.signinbutton}
+      className={styles.signinbutton}
       type="button"
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d361"/>
-      Julio Lima
+      { session.user.name || session.user.email }
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
     
   ) : (
     <button 
-    className={styles.signinbutton}
+      className={styles.signinbutton}
       type="button"
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417"/>
       Sign in with GitHub
