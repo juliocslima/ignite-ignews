1cc306d85022b7483e0d3207d4308f97dc541eed
Julio Lima <lima.julio.cs@gmail.com>
2021-12-08 15:33:40 -0300

    API created, subscribe functions created, FaunaDB functions created and inicialized the webhooks functions concepts

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,7 @@
+import { signIn, useSession } from 'next-auth/react';
+import { toast } from 'react-toastify';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -5,10 +9,35 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+
+  const { data: session } = useSession();
+
+  async function handleSubscribe() {
+    
+    if(!session) {
+      signIn('github');
+      return;
+    }
+
+    // Criação da checkout session
+    try { 
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch(err) {
+      toast.error('Erro na criação da ChackouSession');
+    }
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe
     </button>
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/faunadb';
 
 export default NextAuth({
   providers: [
@@ -8,4 +12,41 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET_KEY,
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile, credentials }) {
+      const { email } = user;
+
+      await fauna.query(
+        q.If(
+          q.Not(
+            q.Exists(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold('lima.julio.cs@gmail.com')
+              )
+            )
+          ),
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          ),
+          q.Get(
+            q.Match(
+              q.Index('user_by_email'),
+              q.Casefold(email)
+            )
+          )
+        )
+      )
+      .then((ret) => {
+        console.log(ret);
+        return true;
+      })
+      .catch((err) => {
+        console.error('Error: %s', err)
+      })
+      
+      return true
+    },
+  }
 })
